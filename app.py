cc0ecba83f586a0601862e86cd0f94b5cb2acae2
BigbyWolf95 <47246572+BigbyWolf95@users.noreply.github.com>
2021-05-14 11:11:42 +0100

    feat: add post request operator for register page to process user registration with checks

--- app.py
+++ app.py
@@ -41,6 +41,37 @@ def view_product(product_id):
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        user_exists = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if user_exists:
+            flash("This Username is taken!")
+            return redirect(url_for("register"))
+        elif request.form.get("password") != request.form.get("password_repeat"):
+            flash("Passwords do not match!")
+            return redirect(url_for('register'))
+        elif not(check_password_hash(os.environ.get("MEADE_PASS"), request.form.get("meade_password"))):
+            flash("Incorrect Meade Passcode!")
+            return redirect(url_for('register'))
+        else:
+            user = {
+                "username": request.form.get("username"),
+                "f_name": request.form.get("f_name"),
+                "l_name": request.form.get("l_name"),
+                "email": request.form.get("email"),
+                "password": generate_password_hash(request.form.get("password")),
+                "department": request.form.get("department"),
+                "role": request.form.get("role")
+            }
+            mongo.db.users.insert_one(user)
+
+            session["user"] = request.form.get("username")
+            flash("Registration Successful")
+            return redirect(url_for("get_upcoming"))
+
+
     departments = list(mongo.db.departments.find().sort([
         ('department_name', pymongo.ASCENDING)
     ]))
