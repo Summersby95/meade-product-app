c6f403e7a20c1e575828db0780ad4952c62988ba
BigbyWolf95 <47246572+BigbyWolf95@users.noreply.github.com>
2021-05-14 14:12:28 +0100

    feat: add log out route and function

--- app.py
+++ app.py
@@ -120,6 +120,16 @@ def login():
 
     return render_template("login.html")
 
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    session.pop("department")
+    session.pop("role")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
