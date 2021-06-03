3ead472fa8f04eb7fdeec0798fe10ff8992ef0b0
BigbyWolf95 <47246572+BigbyWolf95@users.noreply.github.com>
2021-06-03 15:58:17 +0100

    feat: refactor code, create product_mark function in functions.py

--- functions.py
+++ functions.py
@@ -6,3 +6,19 @@ def date_to_string(conv_dict):
             conv_dict[field] = value.strftime("%d %B, %Y")
         elif isinstance(value, dict):
             date_to_string(value)
+
+def product_mark(product):
+    if (product["start_date"].date() < (datetime.now()).date() + timedelta(days=7)):
+        if product["status"] == "Completed - Production Ready":
+            product["colour"] = "green"
+        else:
+            product["colour"] = "red"
+    elif (product["start_date"].date() < (datetime.now()).date() + timedelta(days=14)):
+        if product["status"] != "Completed - Production Ready":
+            product["colour"] = "yellow"
+
+    # Check if the product was created in the last day, 
+    # if it was mark it to add a badge to it in the table
+    if (product["created_on"]).date() + timedelta(days=1) > (datetime.now()).date():
+        product["new"] = True
+
