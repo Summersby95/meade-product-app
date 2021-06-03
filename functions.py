dd930124dbd80cacc556a20b12319b2dabbe62cf
BigbyWolf95 <47246572+BigbyWolf95@users.noreply.github.com>
2021-06-03 15:58:53 +0100

    feat: create process field list function to set select options for field lists

--- functions.py
+++ functions.py
@@ -22,3 +22,17 @@ def product_mark(product):
     if (product["created_on"]).date() + timedelta(days=1) > (datetime.now()).date():
         product["new"] = True
 
+def process_field_list(field_dict, mongo_conn):
+    for value in field_dict.values():
+        if type(value) is dict:
+            process_field_list(value, mongo_conn)
+        elif type(value) is list:
+            for sub_field in value:
+                if sub_field["field_type"] in ["multiselect", "select", "spec_grid"]:
+                    if ("options_type" in sub_field and sub_field["options_type"] == "table") or sub_field["field_type"] == "spec_grid":
+                        options_table = mongo_conn.db[sub_field["table_name"]].find()
+                        sub_field["options"] = []
+                        for option in options_table:
+                            sub_field["options"].append(option["name"])
+                    
+                
