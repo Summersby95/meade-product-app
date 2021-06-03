d9667ece84cc2b1e8b455d6107df6991bc584569
BigbyWolf95 <47246572+BigbyWolf95@users.noreply.github.com>
2021-06-03 16:05:21 +0100

    fix: add created on to field selection in search query

--- app.py
+++ app.py
@@ -252,7 +252,8 @@ def my_tasks():
             "customer": 1,
             "status": 1,
             "start_date": 1,
-            "created_by": 1
+            "created_by": 1,
+            "created_on": 1
         }).sort([
             ('start_date', pymongo.ASCENDING)
         ]))
