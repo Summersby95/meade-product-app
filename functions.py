19a6edc94a19aa286f8b149b69364e5166863930
BigbyWolf95 <47246572+BigbyWolf95@users.noreply.github.com>
2021-06-17 17:41:43 +0100

    fix: PEP8 formatting in functions.py file

--- functions.py
+++ functions.py
@@ -10,30 +10,37 @@ def date_to_string(conv_dict):
 
 
 def product_mark(product):
-    if (product["start_date"].date() < (datetime.now()).date() + timedelta(days=7)):
+    if (product["start_date"].date() < (
+            datetime.now()).date() + timedelta(days=7)):
         if product["status"] == "Completed - Production Ready":
             product["colour"] = "green"
         else:
             product["colour"] = "red"
-    elif (product["start_date"].date() < (datetime.now()).date() + timedelta(days=14)):
+    elif (product["start_date"].date() < (datetime.now()).date() +
+          timedelta(days=14)):
         if product["status"] != "Completed - Production Ready":
             product["colour"] = "yellow"
 
-    # Check if the product was created in the last day, 
+    # Check if the product was created in the last day,
     # if it was mark it to add a badge to it in the table
-    if (product["created_on"]).date() + timedelta(days=1) > (datetime.now()).date():
+    if (product["created_on"]).date() + \
+            timedelta(days=1) > (datetime.now()).date():
         product["new"] = True
 
 
 def process_field_list(field_dict, mongo_conn):
     for value in field_dict.values():
-        if type(value) is dict:
+        if isinstance(value, dict):
             process_field_list(value, mongo_conn)
-        elif type(value) is list:
+        elif isinstance(value, list):
             for sub_field in value:
-                if sub_field["field_type"] in ["multiselect", "select", "spec_grid"]:
-                    if ("options_type" in sub_field and sub_field["options_type"] == "table") or sub_field["field_type"] == "spec_grid":
-                        options_table = mongo_conn.db[sub_field["table_name"]].find()
+                if sub_field["field_type"] in [
+                        "multiselect", "select", "spec_grid"]:
+                    if ("options_type" in sub_field and
+                            sub_field["options_type"] == "table") or \
+                            (sub_field["field_type"] == "spec_grid"):
+                        options_table = \
+                            mongo_conn.db[sub_field["table_name"]].find()
                         sub_field["options"] = []
                         for option in options_table:
                             sub_field["options"].append(option["name"])
@@ -43,29 +50,35 @@ def update_dict_builder(field_list, request_pass, mongo_conn):
     details = {}
     if isinstance(field_list, list):
         for field in field_list:
-            if field["field_type"] == "input" or field["field_type"] == "select":
-                details[field["field_name"]] = request_pass.form.get(field["field_name"])
+            if field["field_type"] == "input" or \
+                    field["field_type"] == "select":
+                details[field["field_name"]] = request_pass.form.get(
+                    field["field_name"])
             elif field["field_type"] == "multiselect":
                 details[field["field_name"]] = []
                 for value in request_pass.form.getlist(field["field_name"]):
                     details[field["field_name"]].append(value)
             elif field["field_type"] == "date":
-                details[field["field_name"]] = datetime.strptime(request_pass.form.get(field["field_name"]), '%d %B, %Y')
+                details[field["field_name"]] = datetime.strptime(
+                    request_pass.form.get(field["field_name"]), '%d %B, %Y')
             elif field["field_type"] == "spec_grid":
                 grid_options = []
                 grid_table = mongo_conn.db[field["table_name"]].find()
 
                 for option in grid_table:
-                    grid_options.append(option["name"].lower().replace(" ", "_"))
-                
+                    grid_options.append(
+                        option["name"].lower().replace(
+                            " ", "_"))
+
                 for option in grid_options:
                     details[option] = {
-                        "rag":request_pass.form.get(option+"_rag"),
-                        "tolerance":request_pass.form.get(option+"_tol"),
-                        "comments":request_pass.form.get(option+"_com")
+                        "rag": request_pass.form.get(option + "_rag"),
+                        "tolerance": request_pass.form.get(option + "_tol"),
+                        "comments": request_pass.form.get(option + "_com")
                     }
     elif isinstance(field_list, dict):
         for sub_field, sub_val in field_list.items():
-            details[sub_field] = update_dict_builder(sub_val, request_pass, mongo_conn)
-    
+            details[sub_field] = update_dict_builder(
+                sub_val, request_pass, mongo_conn)
+
     return details
