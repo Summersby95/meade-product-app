965ad33dea7412076680c761bbed4f9ef3976eef
BigbyWolf95 <47246572+BigbyWolf95@users.noreply.github.com>
2021-06-17 17:39:06 +0100

    feat: create test_vars_exist unittest

--- _test.py
+++ _test.py
@@ -5,3 +5,21 @@ if os.path.exists("env.py"):
     import env
 
 
+def test_vars_exist():
+    envs = [
+        "IP",
+        "PORT",
+        "SECRET_KEY",
+        "MONGO_URI",
+        "MONGO_DBNAME",
+        "MEADE_PASS",
+        "EMAIL_LOGIN",
+        "EMAIL_PASS"]
+    if os.path.exists("env.py"):
+        for x in envs:
+            assert os.getenv(x) is not None, \
+                "Please set environment variable '{}'".format(x)
+    else:
+        assert os.path.exists("env.py"), "Create an env.py file"
+
+
